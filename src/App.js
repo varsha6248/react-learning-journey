eb9d44af0ee3eb5e7c2891e65d753ad841df5e20
varsha6248 <varsha31072005@gmail.com>
2026-06-20 17:16:49 +0530

    learning react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import HabitTrack from './HabitTrack';
+function App(){
+  const habits=[
+    {
+      id:1,
+      habit:"DSA practice",
+      completed:true,
+      time:"1 hour"
+    },
+    {
+      id:2,
+      habit:"React Learning",
+      completed:true,
+      time:"2 hour"
+    },
+    {
+      id:3,
+      habit:"Aptitude",
+      completed:false,
+      time:"1 hour"
+    },
+    {
+      id:4,
+      habit:"Mock Interview",
+      completed:true,
+      time:"1 hour"
+    }
+  ];
+  return(
+    <div className="container">
+      <h1>Daily Habit Tracker</h1>
+      <h3>My Placement Preparation Habits</h3>
+      {
+        habits.map((habit)=>(
+          <HabitTrack key={habit.id} habit={habit.habit} completed={habit.completed} time={habit.time}/>
+        ))
+      }
     </div>
   );
 }
-
 export default App;
---
+++ src/HabitTrack.js
@@ -0,0 +1,22 @@
+import React from 'react';
+function HabitTrack(props){
+    return(
+        <div className="card">
+            <h2>{props.habit}</h2>
+
+            <p>
+                <span style={{color:props.completed?"green":"red",fontweight:"bold",marginLeft:"5px"}}>
+                Status:
+                {
+                    props.completed ? "Completed":"Pending"
+                }
+                </span>
+            </p>
+
+            <p>
+                Time:{props.time}
+            </p>
+        </div>
+    );
+}
+export default HabitTrack;
